e205d4c22b9f31bf9b322d668ba16490d99c8376
Eberson Silva <diretor.ti@byte3ti.com.br>
2023-02-22 15:48:51 -0300

    Atualização de pagina de login

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -11,10 +11,28 @@ class Model {
       $method = substr($name, 0, 3);
 
       $fieldName = substr($name, 3, strlen($name));
+      switch ($method){
 
-      var_dump($method, $fieldName);
-      exit;
+        case 'get':
+          return $this->values[$fieldName];
+          break;
+
+          case 'set':
+            $this->values[$fieldName] = $args[0];
+            break;
+
+      }
   }
+
+  public function setData ($data = array()){
+    foreach($data as $key => $value){
+      $this->{'set' . $key}($value);
+    }
+}
+
+public function getValues(){
+  return $this->values;
+}
 }
 
 ?>
